6f11654df8bbcab3205f127167ee7e63b9598d3b
Kiril <kikodimov2002@gmail.com>
2023-01-21 20:38:34 +0200

    Increased performance by ~1000%

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -1,5 +1,15 @@
 """Image processor"""
 from PIL import Image
+import numpy as np
+
+def replace(target : Image, source : Image):
+    """Replaces 'target' with 'source' in place"""
+    target.resize(source.size)
+    target.paste(source)
+
+def apply_fast(pixels, apply_filter, *args):
+    """Faster implementation"""
+    return apply_filter(pixels, *args)
 
 def apply(img : Image, apply_filter, *args):
     """Applies a filter on a given image"""
@@ -8,106 +18,76 @@ def apply(img : Image, apply_filter, *args):
             pixel = img.getpixel((x, y))
             apply_filter(img, x, y, pixel, *args)
 
-def inverse(img : Image, x, y, pixel):
-    """Inverses a pixel"""
-    img.putpixel((x, y), (255 - pixel[0], 255 - pixel[1], 255 - pixel[2]))
+def inverse_fast(pixels):
+    return 255 - pixels
 
-def grayscale(img : Image, x, y, pixel):
-    """Grayscales a pixel"""
-    gray = round(0.299 * pixel[0]) + round(0.587 * pixel[1]) + round(0.114 * pixel[2])
-    img.putpixel((x, y), (gray, gray, gray))
+def grayscale_fast(pixels):
+    grayVec = [0.299, 0.578, 0.114] # gray vec formula
+    gray = np.dot(pixels[..., :3], grayVec) # 2D gray matrix
+    pixels[..., 0] = gray
+    pixels[..., 1] = gray
+    pixels[..., 2] = gray
+    return pixels
 
-def mono(img : Image, x, y, pixel, *args):
-    """Monochromes a pixel by a given threshold"""
-    gray = round(0.299 * pixel[0]) + round(0.587 * pixel[1]) + round(0.114 * pixel[2])
-
+def mono_fast(pixels, *args):
+    grayVec = [0.299, 0.578, 0.114] # gray vec formula
+    gray = np.dot(pixels[..., :3], grayVec) # 2D gray matrix
+    
     threshold = 127
     if len(args) > 0:
         threshold = 255 - args[0]
+    gray[gray>threshold] = 255
+    gray[gray<=threshold] = 0
+    pixels[..., 0] = gray
+    pixels[..., 1] = gray
+    pixels[..., 2] = gray
+    return pixels
 
-    val = 0 if gray < threshold else 255
-    img.putpixel((x, y), (val, val, val))
+def multiply_fast(pixels, *color):
+    if (len(color) == 0):
+        return pixels
 
-def multiply(img: Image, x, y, pixel, *args):
-    """Multiplies a pixel by a given color (r, g, b)"""
-    if len(args) > 0:
-        color = args[0]
+    a = pixels.astype(np.float64) / 255
+    b =  np.asarray(color) / 255
+    pixels[..., 0] = 255 * a[..., 0] * b[..., 0]  
+    pixels[..., 1] = 255 * a[..., 1] * b[..., 1]  
+    pixels[..., 2] = 255 * a[..., 2] * b[..., 2] 
 
-        a = [pixel[0]/255, pixel[1]/255, pixel[2]/255]
-        b = [color[0]/255, color[1]/255, color[2]/255]
-        change = [
-            255 * a[0] * b[0],
-            255 * a[1] * b[1],
-            255 * a[2] * b[2]]
-        img.putpixel((x,y), (
-            int(change[0]),
-            int(change[1]),
-            int(change[2])
-            ))
+    return pixels
 
-def screen(img: Image, x, y, pixel, *args):
-    """Screens pixel a by a given color (r, g, b)"""
-    if len(args) > 0:
-        color = args[0]
+def screen_fast(pixels, *color):
+    if (len(color) == 0):
+        return pixels
 
-        a = [pixel[0]/255, pixel[1]/255, pixel[2]/255]
-        b = [color[0]/255, color[1]/255, color[2]/255]
-        change = [
-            255 * (1 - (1 - a[0]) * (1 - b[0])),
-            255 * (1 - (1 - a[1]) * (1 - b[1])),
-            255 * (1 - (1 - a[2]) * (1 - b[2]))]
+    a = pixels.astype(np.float64) / 255
+    b =  np.asarray(color) / 255
+    pixels[..., 0] = 255 * (1 - (1 - a[..., 0]) * (1 - b[..., 0]))  
+    pixels[..., 1] = 255 * (1 - (1 - a[..., 1]) * (1 - b[..., 1]))  
+    pixels[..., 2] = 255 * (1 - (1 - a[..., 2]) * (1 - b[..., 2])) 
 
-        img.putpixel((x,y), (
-            int(change[0]),
-            int(change[1]),
-            int(change[2])
-            ))
+    return pixels
 
 #I actually discovered this by accident while trying to figure out how to do 'screen', turns out this is exactly how 'color dodge' works
-def color_dodge(img: Image, x, y, pixel, *args):
-    """Color Dodges a pixel by a given color (r, g, b)"""
-    if len(args) > 0:
-        color = args[0]
+def color_dodge_fast(pixels, *color):
+    if (len(color) == 0):
+        return pixels
+    color = color[0]
 
-        change = [
-            pixel[0] * (255 if color[0] == 255 else 255/(255 - color[0])),
-            pixel[1] * (255 if color[1] == 255 else 255/(255 - color[1])),
-            pixel[2] * (255 if color[2] == 255 else 255/(255 - color[2]))
-        ]
-        img.putpixel((x,y), (
-            int(change[0]),
-            int(change[1]),
-            int(change[2])
-            ))
+    pixels = pixels.astype(np.float64)
 
-def contrast(img: Image, x, y, pixel, *args):
-    """Applies contrast to a pixel by a given threshold"""
-    c = 1
-    if len(args) > 0:
-        c = (args[0]) / 100.0
-    change = [
-        (pixel[0] * c),
-        (pixel[1] * c),
-        (pixel[2] * c),
-    ]
-    img.putpixel((x,y), (
-        int(change[0]),
-        int(change[1]),
-        int(change[2])
-        ))
+    pixels[..., 0] *= 255 if color[0] == 255 else 255/(255 - color[0]) 
+    pixels[..., 1] *= 255 if color[1] == 255 else 255/(255 - color[1]) 
+    pixels[..., 2] *= 255 if color[2] == 255 else 255/(255 - color[2]) 
+    pixels[pixels > 255] = 255
+    return pixels
 
-def brightness(img: Image, x, y, pixel, *args):
-    """Applies brightness to a pixel by a given threshold"""
-    c = 1
-    if len(args) > 0:
-        c = (args[0])
-    change = [
-        (pixel[0] + c),
-        (pixel[1] + c),
-        (pixel[2] + c),
-    ]
-    img.putpixel((x,y), (
-        int(change[0]),
-        int(change[1]),
-        int(change[2])
-        ))
+def contrast_fast(pixels, *args):
+    m = pixels * (args[0] / 100.0)
+    m[m > 255] = 255
+    return m
+
+def brightness_fast(pixels, *args):
+    m = pixels.astype(np.uint16) + args[0]
+    m[m < 0] = 0
+    m[m >= 255] = 255
+    return m
--- program.py
+++ program.py
@@ -2,24 +2,22 @@ import io
 import PySimpleGUI as sg
 from PIL import Image
 import ImageProcessor as ip
+import numpy as np
 
-def update_image(window : sg.Window, image : Image, key='image'):
-    """Updates the window's image with a given image"""
+def update_image(window : sg.Window, pixels : np.ndarray, key='image'):
+    image = Image.fromarray(pixels.astype(np.uint8)).convert('RGB')
     data = io.BytesIO()
     image.save(data, format='PNG')
     window[key].update(data=data.getvalue())
-
-def replace(target : Image, source : Image):
-    """Replaces 'target' with 'source' in place"""
-    target.resize(source.size)
-    target.paste(source)
+    del image
 
 def main():
     """Main method"""
     sg.theme('Dark Grey 8')
 
     original = Image.open('Image.png')
-    image = Image.open('Image.png')
+    orig_pixels = np.array(original)
+    img_pixels = np.copy(orig_pixels)
 
     filters = [
         [sg.Text('Filters:')],
@@ -36,32 +34,34 @@ def main():
         [sg.HorizontalSeparator()],
         [sg.Column(adjustments)],
         [sg.Column(filters)],
-        [sg.Image(size=image.size, key='image')],
+        [sg.Image(size=original.size, key='image')],
     ]
 
     window = sg.Window('Python Image Editor', layout, finalize=True)
-    update_image(window, image)
+    update_image(window, img_pixels)
     while True:
         event, values = window.read()
 
         if event == 'Save':
+            image = Image.fromarray(img_pixels.astype(np.uint8)).convert('RGB')
             image.save('Modified.png')
 
         if event == 'Reset':
-            replace(image, original)
-            update_image(window, image)
+            img_pixels = orig_pixels
+            update_image(window, img_pixels)
 
         if event == 'Inverse':
-            ip.apply(image, ip.inverse)
-            update_image(window, image)
+            img_pixels = ip.apply_fast(img_pixels, ip.inverse_fast)
+            update_image(window, img_pixels)
 
         if event == 'Grayscale':
-            ip.apply(image, ip.grayscale)
-            update_image(window, image)
+            img_pixels = ip.apply_fast(img_pixels, ip.grayscale_fast)
+            update_image(window, img_pixels)
 
         if event in ['Monochrome', 'Contrast', 'Brightness']:
-            copy = Image.new(image.mode, image.size)
-            copy.paste(image)
+            copy_pixels = img_pixels.copy()
+            width = copy_pixels.shape[0]
+            height = copy_pixels.shape[0]
 
             sl_text = 'Amount:'
 
@@ -69,85 +69,85 @@ def main():
                 sl_range = (0, 255)
                 sl_def = 255 / 2
                 sl_text = 'Threshold:'
-                to_apply = ip.mono
-                ip.apply(copy, to_apply, sl_def)
+                to_apply = ip.mono_fast
+                copy_pixels = ip.apply_fast(copy_pixels, to_apply, sl_def)
 
             elif event == 'Contrast':
                 sl_range = (0, 1000)
                 sl_def = 100
-                to_apply = ip.contrast
+                to_apply = ip.contrast_fast
 
             elif event == 'Brightness':
                 sl_range = (-255, 255)
                 sl_def = 0
-                to_apply = ip.brightness
+                to_apply = ip.brightness_fast
 
             layout_popup = [
-                [sg.Image(size=copy.size, key='image')],
+                [sg.Image(size=(width, height), key='image')],
                 [sg.Text(sl_text)],
-                [sg.Slider(range=sl_range, enable_events=True, size=(copy.size[0]/9,20), default_value=sl_def, key='slider', orientation='h')],
-                [sg.Button('OK'), sg.Button('Preview'), sg.Checkbox('Live Preview (laggy)', key='live_preview')]
+                [sg.Slider(range=sl_range, enable_events=True, size=(width/9,20), default_value=sl_def, key='slider', orientation='h')],
+                [sg.Button('OK'), sg.Button('Cancel'), sg.Button('Preview'), sg.Checkbox('Live Preview', key='live_preview', default=True)]
             ]
             popup = sg.Window(event, layout_popup, finalize=True)
 
-            update_image(popup, copy)
-            replace(copy, image)
+            update_image(popup, copy_pixels)
 
             while True:
                 nest_event, nest_values = popup.read()
 
                 if nest_event == 'Preview' or nest_event == 'slider' and bool(nest_values['live_preview']):
-                    ip.apply(copy, to_apply, int(nest_values['slider']))
-                    update_image(popup, copy)
-                    replace(copy, image)
+                    copy_pixels = ip.apply_fast(copy_pixels, to_apply, int(nest_values['slider']))
+                    update_image(popup, copy_pixels)
+                    copy_pixels = img_pixels.copy()
 
                 if nest_event == 'OK':
-                    ip.apply(image, to_apply, int(nest_values['slider']))
-                    update_image(window, image)
+                    img_pixels = ip.apply_fast(img_pixels, to_apply, int(nest_values['slider']))
+                    update_image(window, img_pixels)
                     break
 
-                if nest_event == sg.WINDOW_CLOSED:
+                if nest_event == 'Cancel' or nest_event == sg.WINDOW_CLOSED:
                     break
 
             popup.close()
 
         if event in ['Screen', 'Multiply', 'Color Dodge']:
-            copy = Image.new(image.mode, image.size)
-            copy.paste(image)
-
+            copy_pixels = img_pixels.copy()
+            width = copy_pixels.shape[0]
+            height = copy_pixels.shape[0]
+            
             layout_popup = [
-                [sg.Image(size=copy.size, key='image')],
-                [sg.Text('R:'), sg.Slider(range=(0,255), size=(copy.size[0]/9 - 3,20), key='R', enable_events = True, orientation='h')],
-                [sg.Text('G:'), sg.Slider(range=(0,255), size=(copy.size[0]/9 - 3,20), key='G', enable_events = True, orientation='h')],
-                [sg.Text('B:'), sg.Slider(range=(0,255), size=(copy.size[0]/9 - 3,20), key='B', enable_events = True, orientation='h')],
-                [sg.Button('OK'), sg.Button('Preview'), sg.Checkbox('Live preview (laggy)', key='live_preview')]
+                [sg.Image(size=(width, height), key='image')],
+                [sg.Text('R:'), sg.Slider(range=(0,255), size=(width/9 - 3,20), key='R', enable_events = True, orientation='h')],
+                [sg.Text('G:'), sg.Slider(range=(0,255), size=(width/9 - 3,20), key='G', enable_events = True, orientation='h')],
+                [sg.Text('B:'), sg.Slider(range=(0,255), size=(width/9 - 3,20), key='B', enable_events = True, orientation='h')],
+                [sg.Button('OK'), sg.Button('Cancel'), sg.Button('Preview'), sg.Checkbox('Live preview', key='live_preview', default=True)]
             ]
 
             popup = sg.Window(event, layout_popup, finalize=True)
-            update_image(popup, copy)
+            update_image(popup, copy_pixels)
 
             if event == 'Screen':
-                to_apply = ip.screen
+                to_apply = ip.screen_fast
             elif event == 'Multiply':
-                to_apply = ip.multiply
+                to_apply = ip.multiply_fast
             else:
-                to_apply = ip.color_dodge
+                to_apply = ip.color_dodge_fast
 
             while True:
                 nest_event, nest_values = popup.read()
 
                 if nest_event == 'Preview' or nest_event in ['R', 'G', 'B'] and bool(nest_values['live_preview']):
-                    replace(copy, image)
                     temp_vals = [nest_values['R'], nest_values['G'], nest_values['B'], 255]
-                    ip.apply(copy, to_apply, temp_vals)
-                    update_image(popup, copy)
+                    copy_pixels = ip.apply_fast(copy_pixels, to_apply, temp_vals)
+                    update_image(popup, copy_pixels)
+                    copy_pixels = img_pixels.copy()
 
                 if nest_event == 'OK':
                     temp_vals = [nest_values['R'], nest_values['G'], nest_values['B'], 255]
-                    ip.apply(image, to_apply, temp_vals)
-                    update_image(window, image)
+                    img_pixels = ip.apply_fast(img_pixels, to_apply, temp_vals)
+                    update_image(window, img_pixels)
                     break
-                if nest_event == sg.WINDOW_CLOSED:
+                if nest_event == 'Cancel' or nest_event == sg.WINDOW_CLOSED:
                     break
 
             popup.close()
